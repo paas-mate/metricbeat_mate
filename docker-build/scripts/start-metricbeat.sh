d885c8b4f4df4fc86253f560aeb96b01d67781f0
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2023-02-13 12:04:14 +0800

    modify start-metricbeat config. (#3)

--- docker-build/scripts/start-metricbeat.sh
+++ docker-build/scripts/start-metricbeat.sh
@@ -2,23 +2,43 @@
 
 mkdir -p $METRICBEAT_HOME/logs
 CONF_FILE=$METRICBEAT_HOME/metricbeat.yml
-echo "metricbeat.config.modules" >$CONF_FILE
+echo 'hostfs: "/hostfs"' >$CONF_FILE
+echo "metricbeat.config.modules:" >$CONF_FILE
 echo "- module: system" >>$CONF_FILE
 echo "  period: 10s" >>$CONF_FILE
 echo "  metricsets:" >>$CONF_FILE
 echo "    - cpu" >>$CONF_FILE
 echo "    - load" >>$CONF_FILE
 echo "    - memory" >>$CONF_FILE
-echo "  reload.enabled: true" >>$CONF_FILE
+echo "    - network" >>$CONF_FILE
+echo "    - process" >>$CONF_FILE
+echo "    - process_summary" >>$CONF_FILE
+echo "    - socket_summary" >>$CONF_FILE
+echo "    - socket" >>$CONF_FILE
+echo "  process.include_top_n:" >>$CONF_FILE
+echo "    by_cpu: ${ES_CPU:-5}" >>$CONF_FILE
+echo "    by_memory: ${ES_MEMORY:-5}" >>$CONF_FILE
+echo "- module: system" >>$CONF_FILE
+echo "  period: 1m" >>$CONF_FILE
+echo "  metricsets:" >>$CONF_FILE
+echo "    - filesystem" >>$CONF_FILE
+echo "    - fsstat" >>$CONF_FILE
+echo "  processors:" >>$CONF_FILE
+echo "  - drop_event.when.regexp:" >>$CONF_FILE
+echo "      system.filesystem.mount_point: '^/(sys|cgroup|proc|dev|etc|host|lib|snap) ($|/)'" >>$CONF_FILE
+echo "- module: system" >>$CONF_FILE
+echo "  period: 15m" >>$CONF_FILE
+echo "  metricsets:" >>$CONF_FILE
+echo "    - uptime" >>$CONF_FILE
 echo "setup.template.settings:" >>$CONF_FILE
 echo "  index.number_of_shards: 1" >>$CONF_FILE
 echo "  index.codec: best_compression" >>$CONF_FILE
 echo "setup.kibana:" >>$CONF_FILE
-echo "  host: $KIBANA_HOST" >>$CONF_FILE
+echo "  host: ${KIBANA_HOST:-locahost:5601}" >>$CONF_FILE
 echo "output.elasticsearch:" >>$CONF_FILE
 
 echo -n '  hosts: ["' >>$CONF_FILE
-echo -n $ES_HOST >> $CONF_FILE
+echo -n ${ES_HOST:-locahost:9200} >> $CONF_FILE
 echo '"]' >>$CONF_FILE
 
 echo "processors:" >>$CONF_FILE
